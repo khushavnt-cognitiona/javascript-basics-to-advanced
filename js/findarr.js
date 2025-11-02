c90b7200cb51fbf08b8132e979d58942e4b9df4b
Khushvant Jadhao <khusujadhao329@gmail.com>
2025-11-02 10:11:41 +0530

    Add array find & filter practice code with JS logic

--- js/findarr.js
+++ js/findarr.js
@@ -1,12 +1,29 @@
-let numbers = [2, 5, 11, 8, 22, 17, 4, 9, 30, 14, 3, 27, 6, 19, 25, 12, 1, 40, 7, 18];
+let numbers = [
+  2, 5, 11, 8, 22, 17, 4, 9, 30, 14, 3, 27, 6, 19, 25, 12, 1, 40, 7, 18,
+];
 
 console.log(numbers);
 
-function findEvan(elemnet){
-
-    return elemnet %2===0;
+function findEven(element) {
+  return element % 2 === 0;
 }
 
-let result= numbers.find(findEvan);
-
+let result = numbers.find(findEven);
 console.log(result);
+
+let arr2 = [
+  "Khushvant","Rohan","Amit","Sagar","Pooja",
+  "Vikas","Sneha","Rahul","Sayali","Priya"
+];
+
+function findName(name) {
+  return name.length > 2;
+}
+
+let name2 = arr2.find(findName);
+console.log(name2);
+
+console.log("***********************************************");
+
+let newArr = arr2.filter((element) => element.length > 4);
+console.log(newArr);
